a97cc3db6ad29b526971410a4a9c87aa2803c2e0
mekdad-salloum <mekdad.salloum.work@gmail.com>
2026-02-11 11:22:24 +0300

    Initial Commit

---
+++ Period Library/clsPeriod.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "clsDate.h"
+
+class clsPeriod
+{
+};
---
+++ Period Library/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "clsDate.h"
+#include "clsPeriod.h"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
