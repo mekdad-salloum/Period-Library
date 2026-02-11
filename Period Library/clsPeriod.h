3239e76403e86cea1a8bb77ca7e5bd99facc400c
mekdad-salloum <mekdad.salloum.work@gmail.com>
2026-02-11 11:23:59 +0300

    Create Period Library

--- Period Library/clsPeriod.h
+++ Period Library/clsPeriod.h
@@ -3,4 +3,44 @@
 
 class clsPeriod
 {
+
+private:
+    clsDate _StartDate;
+    clsDate _EndDate;
+
+public:
+
+    clsPeriod(clsDate StartDate, clsDate EndDate)
+    {
+        _StartDate = StartDate;
+        _EndDate = EndDate;
+    }
+
+
+    static bool IsOverlapPeriods(clsPeriod Period1, clsPeriod Period2)
+    {
+        if (
+            clsDate::CompareDates(Period2._EndDate, Period1._StartDate) == clsDate::enDateCompare::Before
+            ||
+            clsDate::CompareDates(Period2._StartDate, Period1._EndDate) == clsDate::enDateCompare::After
+            )
+            return false;
+        else
+            return true;
+    }
+
+    bool IsOverLapWith(clsPeriod Period2)
+    {
+        return IsOverlapPeriods(*this, Period2);
+    }
+
+    void Print()
+    {
+        cout << "Period Start: ";
+        _StartDate.Print();
+
+
+        cout << "Period End: ";
+        _EndDate.Print();
+    }
 };
--- Period Library/main.cpp
+++ Period Library/main.cpp
@@ -6,6 +6,59 @@ using namespace std;
 
 int main()
 {
+    //Create Dates
+    clsDate Date1(1, 1, 2024);
+    clsDate Date2(10, 1, 2024);
+
+    clsDate Date3(5, 1, 2024);
+    clsDate Date4(15, 1, 2024);
+
+    clsDate Date5(20, 1, 2024);
+    clsDate Date6(25, 1, 2024);
+
+    //Create Periods (Constructor)
+    clsPeriod Period1(Date1, Date2);
+    clsPeriod Period2(Date3, Date4);
+    clsPeriod Period3(Date5, Date6);
+
+    cout << "=== Printing Periods ===\n";
+    Period1.Print();
+    cout << endl;
+
+    Period2.Print();
+    cout << endl;
+
+    Period3.Print();
+    cout << endl;
+
+
+    cout << "\n=== Using Static Function IsOverlapPeriods ===\n";
+
+    if (clsPeriod::IsOverlapPeriods(Period1, Period2))
+        cout << "Period1 and Period2 Overlap\n";
+    else
+        cout << "Period1 and Period2 Do NOT Overlap\n";
+
+
+    if (clsPeriod::IsOverlapPeriods(Period1, Period3))
+        cout << "Period1 and Period3 Overlap\n";
+    else
+        cout << "Period1 and Period3 Do NOT Overlap\n";
+
+
+    cout << "\n=== Using Member Function IsOverLapWith ===\n";
+
+    if (Period1.IsOverLapWith(Period2))
+        cout << "Period1 overlaps with Period2\n";
+    else
+        cout << "Period1 does NOT overlap with Period2\n";
+
+
+    if (Period1.IsOverLapWith(Period3))
+        cout << "Period1 overlaps with Period3\n";
+    else
+        cout << "Period1 does NOT overlap with Period3\n";
+
 
     return 0;
 }
